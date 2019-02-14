af0ed41bf0b078853c1f68901f2b121b97cf17ad
horiz0n-zero <antoinefeufeu>
2019-02-14 19:29:31 +0100

    init

---
+++ MetalScreenSaver/MetalScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  MetalScreenSaverView.h
+//  MetalScreenSaver
+//
+//  Created by Antoine Feuerstein on 13/02/2019.
+//  Copyright © 2019 Antoine Feuerstein. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MetalScreenSaverView : ScreenSaverView
+
+@end
